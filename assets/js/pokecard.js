23fd6d0ce6a0bbdde537b3a47787e48c50756cca
Amanda <amandamoreiramoraes16@gmail.com>
2023-09-19 00:08:30 -0300

    feat:(adicionado campos de egg_group, gender e specie dos pokemons) adicionado os endpoints da pokeapi para tais campos

--- assets/js/pokecard.js
+++ assets/js/pokecard.js
@@ -5,7 +5,68 @@ const id = split.slice(1);
 
 const pokecard = document.getElementById("container");
 
-function pokeCard(pokemon) {
+const getGender = (id) => {
+    const gender_url = `https://pokeapi.co/api/v2/gender/${id}/`;
+    return fetch(gender_url)
+        .then((response) => response.json())
+        .then((result) => {return result.name})
+        .catch((error)=>
+        {
+            const erro = "...";
+            return erro;
+        })
+}
+const getSpecie = (id)=>{
+    const specie_url = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
+    return fetch(specie_url)
+    .then((response) => response.json())
+    .then((result) => {return result.genera[7].genus})
+    .catch((error)=>
+    {
+        const erro = "...";
+        return erro;
+    })
+
+}
+const getEggGroup = (id)=>{
+    const eggGroup_url = `https://pokeapi.co/api/v2/egg-group/${id}`;
+    return fetch(eggGroup_url)
+    .then((response) => response.json())
+    .then((result) => {return result.name})
+    .catch((error)=>
+    {
+        const erro = "...";
+        return erro;
+    })
+
+}
+
+async function fetchGender(id) {
+    try {
+        const gender = await getGender(id);
+        return gender;
+    } catch (error) {
+        console.error(`Ocorreu um erro: ${error}`);
+    }
+}
+async function fetchSpecie(id) {
+    try {
+        const specie = await getSpecie(id);
+        return specie;
+    } catch (error) {
+        console.error(`Ocorreu um erro: ${error}`);
+    }
+}
+async function fetchEggGroup(id) {
+    try {
+        const egg_group = await getEggGroup(id);
+        return egg_group;
+    } catch (error) {
+        console.error(`Ocorreu um erro: ${error}`);
+    }
+}
+
+function pokeCard(pokemon,gender,specie,egg_group) {
     return `
     <div class="pokemon_content ${pokemon.type}">
         <div class="pokemon_header">
@@ -41,10 +102,10 @@ function pokeCard(pokemon) {
                     <span class="label">Abilities</span>
                 </div>
                 <div class="inputs">
-                    <span class="input">${pokemon.specie}</span>
-                    <span class="input">${pokemon.height}</span>
-                    <span class="input">${pokemon.weight}</span>
-                    ${pokemon.abilities.map((ability) => `<span class="input">${ability}</span>`).join("")}
+                    <span class="input ${pokemon.type}Color">${specie}</span>
+                    <span class="input">${pokemon.height} M</span>
+                    <span class="input">${pokemon.weight} KG</span>
+                    <div class="abilities ${pokemon.type}Color">${pokemon.abilities.map((ability) => `<span class="input ability">${ability}</span>`).join("")}</div>
                 </div>
 
             </div>
@@ -58,9 +119,9 @@ function pokeCard(pokemon) {
                     <span class="label">Egg Cycle</span>
                 </div>
                 <div class="inputs">
-                    <span class="input">1334649988687</span>
-                    <span class="input">${pokemon.egg_group}</span>
-                    <span class="input">13464</span>
+                    ${gender === 'female'? `<span class="input female">${gender}</span>`: `<span class="input male">${gender}</span>`}
+                    <span class="input">${egg_group}</span>
+                    <span class="input ${pokemon.type}Color">${pokemon.type}</span>
                 </div>
 
 
@@ -69,8 +130,11 @@ function pokeCard(pokemon) {
 }
 
 function loadCard(id) {
-    pokeAPI.GetPokecard(id).then((pokemon) => {
-        const returnHtml = pokeCard(pokemon);
+    pokeAPI.GetPokecard(id).then(async (pokemon) => {
+        const gender = await fetchGender(id);
+        const specie = await fetchSpecie(id);
+        const egg_group = await fetchEggGroup(id);
+        const returnHtml = pokeCard(pokemon,gender,specie,egg_group);
         pokecard.innerHTML += returnHtml;
     })
 }
